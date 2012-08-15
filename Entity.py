7bdf74895c616c7c9037734dcca9c09190b52c66
co <coer@kth.se>
2012-08-15 09:33:25 +0200

    Added all images

---
+++ Entity.py
@@ -0,0 +1,57 @@
+import pygame, Globals, HitHexagon
+class Entity(object):
+	def __init__(self):
+		self.pos = (0.0,0.0)
+		self.velocity = (0.0,0.0)
+		self.maxVelocity = 3
+		self.isVisible = False;
+		self.hitHexagon = HitHexagon.HitHexagon((pygame.Rect(self.pos,
+			(Globals.TILESIZE, Globals.TILESIZE))),
+			0.5)
+		self.game = None
+
+
+	def setPos( self, pos):
+		self.pos = (int(round(pos[0])), int(round(pos[1])))
+
+	def getMidPos( self ):
+		return (self.pos[0] + float(self.spriteSheet.spriteWidth)/2-1,
+				self.pos[1] + float(self.spriteSheet.spriteHeight)/2-1)
+
+	def addPos( self, pos):
+		x = max(self.pos[0] + pos[0],0)
+		y = max(self.pos[1] + pos[1],0)
+		self.pos = (x,y)
+
+	def isInAir( self, level):
+		underFoot = self.hitHexagon.getPointUnderFoot(self.getMidPos())
+		isInAir = (not level.isPixelWall( underFoot ))
+		
+		if(Globals.DEBUG):
+			font = pygame.font.Font(None, 24)
+			text1 = font.render("inAir? %s" %
+					(isInAir), 1, (10, 10, 10))
+			Globals.CANVAS.blit(text1, (20,60))
+		return isInAir
+
+	def addVelocity( self, velocity):
+		xV = self.velocity[0] + velocity[0]
+		yV = self.velocity[1] + velocity[1]
+
+
+		xV = max(-1*self.maxVelocity, xV)
+		yV = max(-1*self.maxVelocity, yV)
+
+		xV = min(self.maxVelocity, xV)
+		yV = min(self.maxVelocity, yV)
+
+		self.velocity = (xV,yV)
+
+	def draw( self, canvasSurface, level ):
+		pygame.drawRect(canvasSurface, (255,0,255), pygame.Rect(pos, (Globals.TILESIZE, Globals.TILESIZE)))
+
+	def act( self, level):
+		return None
+
+	def getCollisions(self, level):
+		return self.hitHexagon.getCollisions(self.getMidPos(), level)
--- Player.py
+++ Player.py
@@ -1,73 +1,17 @@
-import pygame, sys, SpriteSheet, HitHexagon, Level, Globals, soundplay, math
+import pygame, sys, SpriteSheet, HitHexagon, Level, Globals, soundplay, math, Entity
 WALKANIMATION = 7
-class Player( object ):
+class Player(Entity.Entity):
 	def __init__(self, spriteSheet):
+		super(Player, self).__init__()
 		self.controlAcc = 0.3
-		self.pos = (0.0,0.0)
-		self.velocity = (0.0,0.0)
-		self.maxVelocity = 3
-		self.isVisible = False;
 		self.spriteSheet = spriteSheet
 		self.hitHexagon = HitHexagon.HitHexagon((pygame.Rect(self.pos,
 			(self.spriteSheet.spriteWidth, self.spriteSheet.spriteHeight))),
 			0.5)
 		self.facing = SpriteSheet.FACE_RIGHT
-		self.game = None
-
-	"""Interface"""
-	def getPos( self ):
-		return (int(self.pos[0]), int(self.pos[1]))
 
 	def setHPBar( self, bar ):
 		self.HPbar = bar
-
-	def getMidPos( self ):
-		return (self.getPos()[0] + float(self.spriteSheet.spriteWidth)/2-1,
-				self.getPos()[1] + float(self.spriteSheet.spriteHeight)/2-1)
-
-	def setPos( self, pos):
-		self.pos = (int(round(pos[0])), int(round(pos[1])))
-
-	def getIntPoint(self):
-		return (int(round(self.pos[0])), int(round(self.pos[1])))
-
-	def addPos( self, pos):
-		x = max(self.pos[0] + pos[0],0)
-		y = max(self.pos[1] + pos[1],0)
-		self.pos = (x,y)
-
-	def isInAir( self, level):
-		underFoot = self.hitHexagon.getPointUnderFoot(self.getMidPos())
-		isInAir = (not level.isPixelWall( underFoot ))
-		
-		if(Globals.DEBUG):
-			font = pygame.font.Font(None, 24)
-			text1 = font.render("inAir? %s" %
-					(isInAir), 1, (10, 10, 10))
-			Globals.CANVAS.blit(text1, (20,60))
-		
-
-		return isInAir
-
-	def addVelocity( self, velocity):
-		xV = self.velocity[0] + velocity[0]
-		yV = self.velocity[1] + velocity[1]
-
-
-		xV = max(-1*self.maxVelocity, xV)
-		yV = max(-1*self.maxVelocity, yV)
-
-		xV = min(self.maxVelocity, xV)
-		yV = min(self.maxVelocity, yV)
-
-		self.velocity = (xV,yV)
-
-	def setVelocity( self, velocity ):
-
-		self.velocity = velocity
-
-	def setVisibility( self, newVisibility ):
-		self.isVisible = newVisibility
 
 	def draw( self, canvasSurface, level ):
 		print self.pos
@@ -86,7 +30,7 @@ class Player( object ):
 		cutOffV = self.controlAcc*0.6
 		if(self.velocity[0] <=  -cutOffV): self.addVelocity((cutOffV,0))
 		elif(self.velocity[0] >= cutOffV): self.addVelocity((-cutOffV,0))
-		else: self.setVelocity((0,self.velocity[1]))
+		else: self.velocity = (0,self.velocity[1])
 
 	def jump(self):
 		self.velocity= (self.velocity[0],-14)
